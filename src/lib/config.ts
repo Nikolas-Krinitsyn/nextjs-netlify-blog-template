d4c9485c4bcf7301baa62cda0bea5c6f2f088349
Nikolas-Krinitsyn <59940130+Nikolas-Krinitsyn@users.noreply.github.com>
2023-04-25 13:38:17 +0545

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,18 @@
-import config from "../../config.json";
+import configJson from "../../config.json";
 
 type Config = {
   readonly base_url: string;
   readonly site_title: string;
   readonly site_description: string;
-  readonly site_keywords: string[]; // Update the type to string[]
+  readonly site_keywords: string[];
   readonly posts_per_page: number;
   readonly twitter_account: string;
   readonly github_account: string;
 };
 
-export default config as Config;
+export const config: Config = {
+  ...configJson,
+  site_keywords: configJson.site_keywords.map((keyword) => keyword)
+};
+
+export default config;
